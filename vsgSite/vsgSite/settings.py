d6c86be33ff48a4a8fc992698e75df81cf45b3d2
Moss Drake <mxmossman@gmail.com>
2023-01-21 12:23:13 -0800

    Update settings.py
    - Configure time zone
    - add LicenseConfig

--- vsgSite/vsgSite/settings.py
+++ vsgSite/vsgSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'vsgLicense.apps.VsglicenseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
