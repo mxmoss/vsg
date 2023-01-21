5facaf7589757f58115ae2abbdaf687f7ac5dae5
Moss Drake <mxmossman@gmail.com>
2023-01-21 13:14:16 -0800

    Add License views & urls

--- vsgSite/vsgLicense/urls.py
+++ vsgSite/vsgLicense/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /vsgLicense/
     path('', views.index, name='index'),
+    # ex: /vsgLicense/5/
+    path('<int:customer_id>/', views.detail, name='detail'),
+    # ex: /vsgLicense/5/results/
+    path('<int:customer_id>/results/', views.results, name='results'),
+    # ex: /vsgLicense/5/spinup/
+    path('<int:customer_id>/spinup/', views.spinup, name='spinup'),
 ]
--- vsgSite/vsgLicense/views.py
+++ vsgSite/vsgLicense/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Customer, Node
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vsgLicense index.")
+    latest_customer_list = Customer.objects.order_by('-add_date')[:5]
+    output = ', '.join([q.custName_text for q in latest_customer_list])
+    return HttpResponse(output)
+
+def detail(request, customer_id):
+    return HttpResponse("You're looking at customer %s." % customer_id)
+
+def results(request, customer_id):
+    response = "You're looking at the nodes for customer %s."
+    return HttpResponse(node % customer_id)
+
+def spinup(request, customer_id):
+    return HttpResponse("You're spinning up a node for customer %s." % customer_id)
