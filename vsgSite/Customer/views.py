6feca8de83524c8255370096e206ad3830ace7e0
Moss Drake <mxmossman@gmail.com>
2023-01-21 17:15:48 -0800

    Update views.py
    Use django.shortcuts

--- vsgSite/Customer/views.py
+++ vsgSite/Customer/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Customer, Node
-
 
 def index(request):
     latest_customer_list = Customer.objects.order_by('-add_date')[:5]
-    template = loader.get_template('Customer/index.html')
-    context = {
-        'latest_customer_list': latest_customer_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_customer_list': latest_customer_list}
+    return render(request, 'Customer/index.html', context)
+
 
 def detail(request, customer_id):
     return HttpResponse("You're looking at customer %s." % customer_id)
