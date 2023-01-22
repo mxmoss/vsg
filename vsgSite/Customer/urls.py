83051ee12b8fcb395977c3d73b58cce5d0760c27
Moss Drake <mxmossman@gmail.com>
2023-01-21 17:56:48 -0800

    Simplify customer index template
    Use "Customer" namespace

--- vsgSite/Customer/urls.py
+++ vsgSite/Customer/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'Customer'
 urlpatterns = [
     # ex: /Customer/
     path('', views.index, name='index'),
     # ex: /Customer/5/
     path('<int:customer_id>/', views.detail, name='detail'),
-    # ex: /Customer/5/results/
     path('<int:customer_id>/results/', views.results, name='results'),
-    # ex: /Customer/5/spinup/
     path('<int:customer_id>/spinup/', views.spinup, name='spinup'),
+
 ]
