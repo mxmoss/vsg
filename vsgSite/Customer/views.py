ad277cc6c1667e798cfe33e0fa81689a06a2331b
Moss Drake <mxmossman@gmail.com>
2023-01-24 10:24:52 -0800

    Update views.py
    Improve on handling temp file execution

--- vsgSite/Customer/views.py
+++ vsgSite/Customer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Customer, Node
-import subprocess
+from .models import Customer
+from .utils import *
 
 def index(request):
     latest_customer_list = Customer.objects.order_by('-add_date')[:5]
@@ -13,8 +13,15 @@ def detail(request, customer_id):
     return render(request, 'Customer/detail.html', {'customer': customer})
 def spinup(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
+    customer_txt = str(customer_id)
     if request.POST:
-        subprocess.call('foo.bat', customer_id)
+        fn = tmpFileName(customer_txt)
+        try:
+            exec(fn, customer_txt)
+        except:
+            print("Something went wrong")
+        finally:
+            os.remove(fn)
     return render(request, 'Customer/detail.html', {'customer': customer})
 
 def results(request, customer_id):
