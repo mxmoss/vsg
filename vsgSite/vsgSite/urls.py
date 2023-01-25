847cbaed95e0d83e9d062c5a52f2d88c980c4db8
Moss Drake <mxmossman@gmail.com>
2023-01-24 16:03:21 -0800

    Update urls.py
    Default to Customer list for now

--- vsgSite/vsgSite/urls.py
+++ vsgSite/vsgSite/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('Customer/', include('Customer.urls')),
     path('admin/', admin.site.urls),
+    path('', include('Customer.urls')),
 ]
