eda7ee237578ef73349d73395e0bb1d717f4f08a
Moss Drake <mxmossman@gmail.com>
2023-02-08 14:11:58 -0800

    Update admin.py
    Add proxy to the admin site

--- vsgSite/Customer/admin.py
+++ vsgSite/Customer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Customer, License
+from .models import Customer, License, Proxy
 
 admin.site.register(Customer)
 admin.site.register(License)
+admin.site.register(Proxy)
