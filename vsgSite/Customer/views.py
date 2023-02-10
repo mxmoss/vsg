f105fa6b9a0bf8d98e1b9f9ac8644a491ca791da
Moss Drake <mxmossman@gmail.com>
2023-02-09 17:18:14 -0800

    Update views.py
    Require login to see customer info

--- vsgSite/Customer/views.py
+++ vsgSite/Customer/views.py
@@ -5,29 +5,28 @@ from django.views import generic
 from .models import Customer, License
 from .utils import *
 from django.conf import settings
-#from vsgSite.settings import BASE_DIR, STATIC_URL
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class IndexView(generic.ListView):
+
+class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = 'Customer/index.html'
     context_object_name = 'latest_customer_list'
 
     def get_queryset(self):
         return Customer.objects.order_by('-add_date')[:5]
 
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Customer
     template_name = 'Customer/detail.html'
-#    context_object_name = 'latest_license_list'
-#    def get_queryset(self):
-#        return License.objects.order_by('-add_date')
 
-class ResultsView(generic.DetailView):
+class ResultsView(LoginRequiredMixin, generic.DetailView):
     model = Customer
     template_name = 'Customer/results.html'
 
 def spinup(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
     customer_txt = str(customer_id)
+
     if request.POST:
         src = os.path.join(settings.STATIC_PATH,'AWSProxy.bat')
         dest = tmpFileName(customer_txt)
